8763314bb621e3ff7034cb13e74daab77165a352
smaslyk <steven.maslyk@gmail.com>
2026-09-24 00:13:35 -0500

    removed :authority

--- src/pages/debug/headers.ts
+++ src/pages/debug/headers.ts
@@ -1,7 +1,8 @@
-export const GET = ({ request }) =>
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = ({ request }) =>
   Response.json({
     host: request.headers.get("host"),
-    authority: request.headers.get(":authority"),
     forwardedHost: request.headers.get("x-forwarded-host"),
     origin: request.headers.get("origin"),
     referer: request.headers.get("referer"),
