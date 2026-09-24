0e9522b57a0d4795088846472f11e59dec670af5
smaslyk <steven.maslyk@gmail.com>
2026-09-23 23:43:07 -0500

    Disabled security origin bypass

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  security: {
-    checkOrigin: false
-  },
   adapter: node({
     mode: 'standalone',
   }),
