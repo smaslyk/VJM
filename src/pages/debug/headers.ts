46567855c5fb4065bb97d23b93064ef2b00e304d
smaslyk <steven.maslyk@gmail.com>
2026-09-24 00:23:40 -0500

    added request url and proto

--- src/pages/debug/headers.ts
+++ src/pages/debug/headers.ts
@@ -2,8 +2,9 @@ import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ request }) =>
   Response.json({
+    requestUrl: request.url,
     host: request.headers.get("host"),
     forwardedHost: request.headers.get("x-forwarded-host"),
+    forwardedProto: request.headers.get("x-forwarded-proto"),
     origin: request.headers.get("origin"),
-    referer: request.headers.get("referer"),
   });
